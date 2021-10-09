3c5589806eac4f7f155f85226c4fee9dff57727f
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-10-09 13:16:13 +0200

    ready with setup

---
+++ sound-check-app/src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-xl font-bold text-blue-500">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- sound-check-app/svelte.config.js
+++ sound-check-app/svelte.config.js
@@ -1,7 +1,15 @@
-import sveltePreprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
 
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
