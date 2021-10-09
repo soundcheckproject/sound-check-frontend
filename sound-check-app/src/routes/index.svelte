fd23e0683fa097cb672b0a5d333a7dfb00b87c12
Maxime <maxime.vermeeren@student.howest.be>
2021-10-09 13:30:39 +0200

    Add 404, navigation component and multiple routes

--- sound-check-app/src/routes/index.svelte
+++ sound-check-app/src/routes/index.svelte
@@ -1,2 +1,12 @@
+<script>
+    let click = 0;
+
+    function addClick() {
+        click +=1
+    }
+</script>
+
+
 <h1 class="text-xl font-bold text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={addClick} class="p-4 bg-red-300">{click}</button>                
