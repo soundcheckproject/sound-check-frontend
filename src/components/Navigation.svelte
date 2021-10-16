3ef510c007379097692db519c6935ab449f0438b
Maxime <maxime.vermeeren@student.howest.be>
2021-10-16 17:44:36 +0200

    Add HTTPS, login & register page, nav components

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { generalStore } from '../stores/stores';
+</script>
+
+<div>
+	<div>{$generalStore.Name} :)</div>
+	<div />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,8 @@
-<script>
-    let click = 0;
-
-    function addClick() {
-        click +=1
-    }
-</script>
-
-
 <h1 class="text-xl font-bold text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click={addClick} class="p-4 bg-red-300">{click}</button>                
+
+<div class="flex p-4 flex-col">
+	<a href="/index">/index</a>
+	<a href="/login">/login</a>
+	<a href="/register">/register</a>
+</div>
