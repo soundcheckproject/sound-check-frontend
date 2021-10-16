529fb2a3a00253601516b17312ddadfc3edb5bf7
Maxime <maxime.vermeeren@student.howest.be>
2021-10-16 20:15:17 +0200

    adding custom font roboto & gotham

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,11 @@
-<slot />
+<div class="font-roboto">
+	<slot />
+</div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+	@import url('../assets/fonts/gotham/style.css');
+
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,17 @@
 module.exports = {
 	mode: 'jit',
-	purge: ['./src/**/*.svelte']
+	purge: ['./src/**/*.svelte'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+				gotham: ['Gotham', 'sans-serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
