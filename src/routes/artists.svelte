ff75b0bdc9d95e7bfb42c0e81653efdf94883cd3
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-11-29 14:41:59 +0100

    filter input styling

--- src/routes/artists.svelte
+++ src/routes/artists.svelte
@@ -8,6 +8,7 @@
   import { each } from 'svelte/internal'
 import ArtistCard from '../components/ArtistCard.svelte'
 import ArtistCardSkeleton from '../components/skeleton/ArtistCardSkeleton.svelte'
+import Footer from '../components/Footer.svelte'
 
   let artists: UserType[] = []
 
@@ -31,11 +32,16 @@ import ArtistCardSkeleton from '../components/skeleton/ArtistCardSkeleton.svelte
   })
 </script>
 
-<Header type="split" />
-<div class="bg-gray-300">
-  <Container>
-    <input type="text" name="name" id="name" />
-    <select name="filter" id="filter" />
+<Header type="split"/>
+<main>
+<div class="bg-gray-100 shadow-sm">
+    <Container className="py-4">
+      <form class="relative">
+        <input type="text" class="p-2 pl-9 rounded-sm border-2 border-gray-200 outline-none transition-colors bg-gray-50 max-w-min hover:border-teal-700 active:border-teal-700 focus:border-teal-700 m-0" placeholder="Artist name">
+        <button type="submit">
+          <svg class="w-5 h-5 absolute top-3 left-2 bottom-3 text-gray-900  stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+        </button>
+      </form>
   </Container>
 </div>
 <div>
@@ -48,9 +54,10 @@ import ArtistCardSkeleton from '../components/skeleton/ArtistCardSkeleton.svelte
         {:else}
         {#each Array(12) as i}
         <ArtistCardSkeleton/>
-            
         {/each}
       {/if}
     </section>
   </Container>
 </div>
+</main>
+<Footer/>
