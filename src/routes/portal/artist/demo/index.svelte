66ef431d0af1c687c402fb8f2d472ec2ef8efd0d
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-12-08 19:53:50 +0100

    ✅ Staff tracks responsive

--- src/routes/portal/artist/demo/index.svelte
+++ src/routes/portal/artist/demo/index.svelte
@@ -90,7 +90,7 @@
   <Box>
     <Title>
       <div class="grid sm:grid-cols-2 items-center">
-        <div>All tracks</div>
+        <h1>All tracks</h1>
         <div class="flex space-x-2 sm:ml-auto">
           <select class="portal input w-32" bind:value={filterType}>
             <option value="all">All</option>
