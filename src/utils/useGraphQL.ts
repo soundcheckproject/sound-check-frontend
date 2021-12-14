aca964080c0616f6be54555d89ef51d410e17012
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-12-14 12:14:33 +0100

    ✅ Retry button

--- src/utils/useGraphQL.ts
+++ src/utils/useGraphQL.ts
@@ -203,7 +203,7 @@ export const getAllTracks = async (): Promise<TrackType[]> => {
       getTracks {
         uuid
         title
-        description
+        dedscription
         lyrics
         contractFile
         resource
