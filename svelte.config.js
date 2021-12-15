a04ce9af7ef4a0724fa62947b4882088821cb33b
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-12-15 13:38:14 +0100

    static site instead of node site

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-node'
+// import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,24 +11,24 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({
-      out: 'build',
-      precompress: false,
-      env: {
-        host: 'NODE_HOST',
-        port: 'NODE_PORT',
-      },
-    }),
-    //     adapter: adapter({
-    //       // default options are shown
-    //       pages: 'build',
-    //       assets: 'build',
-    //       fallback: null,
-    //       ssr: false,
-    //     }),
-    //     prerender: {
-    //       enabled: false,
-    //     },
+    // adapter: adapter({
+    //   out: 'build',
+    //   precompress: false,
+    //   env: {
+    //     host: 'NODE_HOST',
+    //     port: 'NODE_PORT',
+    //   },
+    // }),
+        adapter: adapter({
+          // default options are shown
+          pages: 'build',
+          assets: 'build',
+          fallback: null,
+          ssr: false,
+        }),
+        prerender: {
+          enabled: false,
+        },
   },
 }
 
